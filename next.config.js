e117c5e6c495c13adc5a18168b35d09b8b62a4aa
Vincent <khung.5408@gmail.com>
2025-11-16 21:57:24 -0500

    X robot tag

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
   output: "standalone",
+  async headers() {
+    return [
+      {
+        // Target your sitemap file
+        source: '/sitemap.xml',
+        headers: [
+          {
+            // Explicitly set the X-Robots-Tag to allow indexing and following
+            key: 'X-Robots-Tag',
+            value: 'index, follow',
+          },
+        ],
+      },
+    ];
+  },
 };
